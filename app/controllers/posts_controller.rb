152436bf4c82849e7ffb366555509548a4dd5979
weare138 <readfuzza@gmail.com>
2014-11-20 12:25:05 +0300

    formatting code

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,4 @@
-	class PostsController < ApplicationController
+class PostsController < ApplicationController
 	
 	def index
 		@posts = Post.all
@@ -10,9 +10,9 @@
 
 	def create
 		@post = Post.new(post_params)
-        if @post.save
-            redirect_to root_url
-        end
+      if @post.save
+        redirect_to root_url
+      end
 	end
 
 	def show
@@ -26,23 +26,23 @@
      end
 
     def edit
-   	    @post = Post.find(params[:id])
+   	  @post = Post.find(params[:id])
 
     end
 
 	def update
-	    @post = Post.find(params[:id])
-         if @post.update(post_params)
-            redirect_to @post
-        else
-            render 'edit'
-        end
+	  @post = Post.find(params[:id])
+      if @post.update(post_params)
+        redirect_to @post
+      else
+        render 'edit'
+      end
 	end
 
 	private
 
     def post_params
-        params.require(:post).permit(:title, :body)
+      params.require(:post).permit(:title, :body)
     end
 end
 
