3f5cf34fc4cde8e7ac606da9008bccf6a1265ff5
weare138 <readfuzza@gmail.com>
2014-11-25 21:36:34 +0300

    first commit

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ActiveRecord::Base
+  has_many :comments
   validates :title, :body, presence: true
   mount_uploader :photo, PhotoUploader
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   root 'welcome#index'
-
   resources :images
-    get '/upload_images' => 'images#upload_images', as: :upload_images
-    post '/upload' => 'images#upload', as: :upload
-
-  resources :posts
-
+  get '/upload_images' => 'images#upload_images', as: :upload_images
+  post '/upload' => 'images#upload', as: :upload
+  resources :posts do
+    resources :comments
+  end
 end
