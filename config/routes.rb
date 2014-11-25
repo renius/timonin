ad3348beef65d1db7a23feae17f2890b58f571f8
weare138 <readfuzza@gmail.com>
2014-11-25 21:42:47 +0300

    remove css js helper

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   root 'welcome#index'
+
   resources :images
-  get '/upload_images' => 'images#upload_images', as: :upload_images
-  post '/upload' => 'images#upload', as: :upload
+    get '/upload_images' => 'images#upload_images', as: :upload_images
+    post '/upload' => 'images#upload', as: :upload
+
   resources :posts do
     resources :comments
   end
