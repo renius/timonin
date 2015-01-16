97e498f3e212da7108ce7c2a509561536ec17548
weare138 <readfuzza@gmail.com>
2015-01-16 15:21:13 +0300

    travis
    travis
    
    Update README.rdoc
    
    Update README.rdoc
    
    Update .travis.yml
    
    Update .travis.yml
    
    Update .travis.yml
    
    Update .travis.yml
    
    Rename database.yml.example to database.yml.
    
    Rename database.yml. to database.yml
    
    Rename .travis.yml to .travis.yml.example
    
    Update .travis.yml.example
    
    Update .travis.yml.example
    
    Rename database.yml to database.yml.example
    
    Rename .travis.yml.example to .travis.yml
    
    Update .travis.yml
    
    Update .travis.yml
    
    Update database.yml.example
    
    Rename .travis.yml to .travis.ym
    
    Rename .travis.ym to .travis.yml
    
    Update .travis.yml
    
    Update .travis.yml
    
    Update database.yml.example
    
    Update database.yml.example
    
    Update database.yml.example
    
    Update database.yml.example
    
    Update database.yml.example
    
    Update database.yml.example
    
    Update database.yml.example
    
    Update database.yml.example
    
    Update .travis.yml
    
    pend failed test
    
    travis

--- spec/support/macros.rb
+++ spec/support/macros.rb
@@ -1,8 +1,8 @@
 module RequestMacros
   include Warden::Test::Helpers
 
-  def sign_in_as_a_user
-    @user ||= FactoryGirl.create(:user, role: 'admin')
+  def sign_in_as_a_user(args = {})
+    @user ||= FactoryGirl.create(:user, args[:role])
     login_as @user
   end
 end
