5a7541a521177ff3ed338f0bb35891a27eebb7fd
weare138 <readfuzza@gmail.com>
2015-01-16 15:21:13 +0300

    travis

--- spec/requests/admin/admin_posts_spec.rb
+++ spec/requests/admin/admin_posts_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "Admin::Posts", :type => :request do
   describe "GET /admin_posts" do
     it "works! (now write some real specs)" do
-      sign_in_as_a_user
+      sign_in_as_a_user(role: :admin)
       get admin_posts_path
       expect(response).to have_http_status(200)
     end
--- spec/support/macros.rb
+++ spec/support/macros.rb
@@ -1,8 +1,8 @@
 module RequestMacros
   include Warden::Test::Helpers
 
-  def sign_in_as_a_user
-    @user ||= FactoryGirl.create(:user, role: 'admin')
+  def sign_in_as_a_user(role = {role: nil})
+    @user ||= FactoryGirl.create(:user, role.fetch(:role))
     login_as @user
   end
 end
