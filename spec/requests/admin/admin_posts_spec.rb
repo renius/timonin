28a3d1ae97a776991b4125bee427f59ca1235a09
weare138 <readfuzza@gmail.com>
2015-01-16 15:21:13 +0300

    fix tests

--- spec/requests/admin/admin_posts_spec.rb
+++ spec/requests/admin/admin_posts_spec.rb
@@ -3,9 +3,15 @@ require 'rails_helper'
 RSpec.describe "Admin::Posts", :type => :request do
   describe "GET /admin_posts" do
     it "works! (now write some real specs)" do
-      sign_in_as_a_user
+      sign_in_as_a_user(role: :admin)
       get admin_posts_path
       expect(response).to have_http_status(200)
+    end
+
+    it "don't works!" do
+      sign_in_as_a_user
+      get admin_posts_path
+      expect(response).to redirect_to(root_url)
     end
   end
 end
